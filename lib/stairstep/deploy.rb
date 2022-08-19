6b6abe6591869f9c1bd21f72fe0d1a994aa75b42
Adam Milligan <adam@redish.com>
2022-08-19 10:09:07 -0400

    Use atomic file utility method
    Authored-by: Adam Milligan <adam@redish.com>

--- lib/stairstep/deploy.rb
+++ lib/stairstep/deploy.rb
@@ -42,7 +42,7 @@ class Stairstep::Deploy < Stairstep::Base
   end
 
   def ensure_log_directory
-    Dir.mkdir("log") unless Dir.exist?("log")
+    FileUtils.mkdir_p
   end
 
   def prepare_commit(ref_name)
